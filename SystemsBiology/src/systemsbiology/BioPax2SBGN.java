2a96c4c8da60fa816ba7df505cc6a41a22266113
Samuel Benavides <samuel.benavides.garcia@gmail.com>
2015-10-26 16:29:44 +0100

    BioPax2SBGN

---
+++ SystemsBiology/src/systemsbiology/BioPax2SBGN.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package systemsbiology;
+
+import java.io.File;
+
+/**
+ *
+ * @author Samuel Benavides García
+ */
+public class BioPax2SBGN {
+  public File bioPax2SBGN(String filein){
+    return null;
+  }
+}
